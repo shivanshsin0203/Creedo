8cd4250c9ae90df970c790e317784530c6f7128f
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-26 16:28:39 +0530

    Added logout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,5 @@
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";const {getUser} = getKindeServerSession();
+import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
    let user:any; 
 async function getuser() {
  user =  await getUser();
@@ -7,8 +8,10 @@ console.log(user);
     getuser();
 const page = () => {
   return (
-    
+    <>
+    <LogoutLink className=" h-[48px] w-[120px] bg-blue-600 text-white">Log out</LogoutLink>
     <div>{user.given_name}</div>
+    </>
   )
 }
 
