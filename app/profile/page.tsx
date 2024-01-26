96c1d6c57addd0071e1412a2653a11355928521c
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-26 16:42:33 +0530

    Testing loading

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,20 @@
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";const {getUser} = getKindeServerSession();
-import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
-   let user:any; 
-async function getuser() {
- user =  await getUser();
-console.log(user);
-    }
-    getuser();
-const page = () => {
-  return (
-    <>
-    <LogoutLink className=" h-[48px] w-[120px] bg-blue-600 text-white">Log out</LogoutLink>
-    <div>{user.given_name}</div>
-    </>
-  )
-}
+"use client";
 
-export default page
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+export default function Admin() {
+  const { isAuthenticated, isLoading } = useKindeBrowserClient();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return isAuthenticated ? (
+    <div>Admin content
+    <LogoutLink className=" h-[48px] w-[120px] bg-blue-600 text-white">Log out</LogoutLink>
+    </div>
+  ) : (
+    <div>
+      You have to <LoginLink>Login</LoginLink> to see this page
+    </div>
+  );
+}
