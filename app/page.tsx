c17f15f33a8718b08b4625e9e345b0c0e43e52dc
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-26 20:58:21 +0530

    Added layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 const page = () => {
   return (
     <div className=" bg-white w-full h-full flex flex-col items-center justify-center space-y-5">
-    <h1 className=" font-extrabold text-xl text-black">Login page b the time</h1>
+    <h1 className=" font-extrabold text-xl text-black">Login page by the time</h1>
       
   <LoginLink className=" h-[48px] w-[120px] bg-blue-600 text-white">Sign in</LoginLink>
   <RegisterLink className="h-[48px] w-[120px] bg-blue-600 text-white">Sign up</RegisterLink>
